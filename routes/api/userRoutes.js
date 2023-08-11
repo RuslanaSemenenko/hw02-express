b9f7a7194be25ab1b7ef912d251f84cc780996f1
Ruslana Semenenko <katotochkaaa@gmail.com>
2023-08-11 18:02:56 +0100

    homework

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 
-const userController = require("../controllers/users"); 
+const userController = require("../controllers/users");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
