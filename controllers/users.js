3c91a2a637930d9b69e1cc7f8b554145b11e4992
Ruslana Semenenko <katotochkaaa@gmail.com>
2023-08-12 13:25:23 +0100

    homework

--- controllers/users.js
+++ controllers/users.js
@@ -3,9 +3,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../../models/user");
 const { HttpError } = require("../helpers/HttpErrors");
 const gravatar = require("gravatar");
-const path = require("path");
-const fs = require("fs").promises;
-const Jimp = require("jimp");
 const { registrationSchema } = require("../schemas/validationSchemas");
 
 const registerUser = async (req, res, next) => {
@@ -39,28 +36,6 @@ const registerUser = async (req, res, next) => {
   }
 };
 
-const uploadAvatar = async (req, res, next) => {
-  try {
-    const avatarFile = req.file;
-
-    const image = await Jimp.read(avatarFile.path);
-    await image.resize(250, 250).write(avatarFile.path);
-
-    const newFileName = path.basename(avatarFile.path);
-    const newFilePath = path.join("public/avatars", newFileName);
-    await fs.rename(avatarFile.path, newFilePath);
-
-    const user = await User.findById(req.user.id);
-    user.avatarURL = `/avatars/${newFileName}`;
-    await user.save();
-
-    res.json({ avatarURL: user.avatarURL });
-  } catch (error) {
-    next(error);
-  }
-};
-
 module.exports = {
   registerUser,
-  uploadAvatar,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,11 +4,12 @@ const {
   logoutUser,
   getCurrentUser,
 } = require("../controllers/auth");
-const authMiddleware = require("../controllers/auth");
+const authMiddleware = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
 router.post("/login", loginUser);
-router.post("/logout", authMiddleware, logoutUser);
-router.get("/current", authMiddleware, getCurrentUser);
+router.post("/logout", authMiddleware.verifyToken, logoutUser);
+router.get("/current", authMiddleware.verifyToken, getCurrentUser);
 
 module.exports = router;
